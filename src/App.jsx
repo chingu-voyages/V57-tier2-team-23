74f24d178eb5a391b96dc611a23a099c05b255e6
Koby <kobysysouvanh@gmail.com>
2025-09-10 19:10:31 -0400

    feat: integrate Shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="w-full h-screen flex justify-center items-center flex-col">
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="flex justify-center flex-col">
+        <Button className="hover:cursor-pointer" onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p className="">
+        </Button>
+        <p >
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p>
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
