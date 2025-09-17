b1a79f177267bd15b552c4631fcbfbb8de9e4b49
Francisco Cuevas <124302532+fcuevas6@users.noreply.github.com>
2025-09-17 08:55:47 -0400

    Clean up App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import { useState } from "react";
 import { Button } from "./components/ui/button";
 
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="w-full h-screen flex justify-center items-center flex-col">
-      <h1>Vite + React</h1>
+      <h1>PR Status Board</h1>
       <div className="flex justify-center flex-col">
-        <Button className="hover:cursor-pointer" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <Button className="hover:cursor-pointer">
+          Get Started
         </Button>
-        <p >
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+        </div>
     </div>
   );
 }
