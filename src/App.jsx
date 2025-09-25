92929424ebdd456647967c3cfab8ddd74da23e28
Koby <kobysysouvanh@gmail.com>
2025-09-25 19:32:26 -0400

    Integrate Toaster for notifications. Add input fields and button for repo and two buttons for loading/download JSON data.

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,39 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LookupPage from "./pages/LookupPage";
+import { Toaster } from "./components/ui/sonner";
 
 const router = createBrowserRouter([
-    {
-      path:'/',
-      element: (
-        <div className="h-screen flex flex-col overflow-y-hidden">
-          <Header />
-          <HomePage />
-          <Footer />
-        </div>
-      ),
-    },
+  {
+    path: "/",
+    element: (
+      <div className="h-screen flex flex-col overflow-y-hidden">
+        <Header />
+        <HomePage />
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: "/lookup",
+    element: (
+      <div className="h-screen flex flex-col">
+        <Toaster />
+        <Header />
+        <LookupPage />
+        <Footer />
+      </div>
+    ),
+  },
 ]);
 
 function App() {
-
   return (
-    <div className='App'>
-        <RouterProvider router={router} />
+    <div className="App">
+      <RouterProvider router={router} />
     </div>
-  );  
+  );
 }
 
 export default App;
