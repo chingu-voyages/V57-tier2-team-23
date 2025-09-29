b87b4c7103a73a5faedc4fbeb413fc75afa28a15
Koby <kobysysouvanh@gmail.com>
2025-09-29 18:28:32 -0400

    Refactor PRCard component and enhance LookupPage with PR fetching and JSON import/export functionality

--- src/components/PrCard.jsx
+++ src/components/PrCard.jsx
@@ -1,32 +1,41 @@
 import React from "react";
-import { 
-    Card,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-    CardContent
- } from '@/components/ui/card';
- import { 
-    GitPullRequestArrow,
-    UserRound,
-    UsersRound,
-    CheckCheck
- } from 'lucide-react'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import {
+  GitPullRequestArrow,
+  UserRound,
+  UsersRound,
+  CheckCheck,
+} from "lucide-react";
 
-const PRCard = ({ title, prNum, owner, assignees, lastAction }) => {
-    return (
-        <Card className="w-full p-4 shadow-md border border-gray-200 rounded-lg" >
-            <CardHeader className="flex justify-between mb-2">
-                <CardTitle className="flex items-center gap-2"><GitPullRequestArrow className="stroke-green-400" />{title}</CardTitle>
-                <CardDescription>#{prNum}</CardDescription>
-            </CardHeader>
-            <CardContent className="pb-6">
-                <CardDescription className="flex items-center gap-2 pb-3"><UserRound />Created by: {owner}</CardDescription>
-                <CardDescription className="flex items-center gap-2 pb-3"><UsersRound />Reviewers: {assignees.join(', ')}</CardDescription>
-                <CardDescription className="flex items-center gap-2 pb-3"><CheckCheck className="stroke-green-400"/>Last Action: {lastAction}</CardDescription>
-            </CardContent>
-        </Card>
-    )
-}
+const PRCard = ({ pr }) => {
+  return (
+    <Card className="w-full p-4 shadow-lg border border-gray-200 rounded-lg">
+      <CardHeader className="flex justify-between mb-2">
+        <CardTitle className="flex items-center  gap-2">
+          <GitPullRequestArrow className="stroke-green-400" />
+          <a href={pr.html_url} className="hover:text-[#58A6FF] transition-all duration-150">{pr.title}</a>
+        </CardTitle>
+        <CardDescription>#{pr.number}</CardDescription>
+      </CardHeader>
+      <CardContent className="pb-6">
+        <CardDescription className="flex items-center gap-2 pb-3">
+          <UserRound />
+          Created by: {pr.user.login}
+        </CardDescription>
+        <CardDescription className="flex items-center gap-2 pb-3">
+        </CardDescription>
+        <CardDescription className="flex items-center gap-2 pb-3">
+          <CheckCheck className="stroke-green-400" />
+        </CardDescription>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default PRCard;
